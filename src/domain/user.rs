55422ed3d5aef890e677d47202b14c8f17764c08
Laure-Riglet <laure.riglet@gmail.com>
2026-02-04 23:34:24 +0100

    refactor(domain): user:  update User struct to use DateTime for timestamps and make password public

--- src/domain/user.rs
+++ src/domain/user.rs
@@ -1,28 +1,27 @@
+use chrono::{DateTime, Utc};
+
 #[derive(Debug, Clone)]
 pub struct User {
-    pub id: i32,
     pub username: String,
     pub email: String,
-    password: String,
+    pub password: String,
     pub tz_continent: String,
     pub tz_city: String,
-    pub created_at: String,
-    pub updated_at: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
 }
 
 impl User {
     pub fn new(
-        id: i32,
         username: String,
         email: String,
         password: String,
         tz_continent: String,
         tz_city: String,
-        created_at: String,
-        updated_at: String,
+        created_at: DateTime<Utc>,
+        updated_at: DateTime<Utc>,
     ) -> Self {
         Self {
-            id,
             username,
             email,
             password,
@@ -31,31 +30,5 @@ impl User {
             created_at,
             updated_at,
         }
-    }
-
-    // Getters
-    pub fn id(&self) -> i32 {
-        self.id
-    }
-    pub fn username(&self) -> &str {
-        &self.username
-    }
-    pub fn email(&self) -> &str {
-        &self.email
-    }
-    pub fn password(&self) -> &str {
-        &self.password
-    }
-    pub fn tz_continent(&self) -> &str {
-        &self.tz_continent
-    }
-    pub fn tz_city(&self) -> &str {
-        &self.tz_city
-    }
-    pub fn created_at(&self) -> &str {
-        &self.created_at
-    }
-    pub fn updated_at(&self) -> &str {
-        &self.updated_at
     }
 }
